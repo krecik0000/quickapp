52c0c7e9c9f6d905441549968df2d8678efc3e63
krecik0000 <milo04v2@gmail.com>
2020-08-04 17:43:38 +0200

    rebuild

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 from Tkinter import *
+import tkFont
 import webbrowser
 import os
 #from googlesearch import *
 
 root = Tk()
 root.title("Menu")
+root.configure(bg='#2b2b2b')
 #root.geometry('745x120')
 root.resizable(width=0, height=0)
-#def search():
-#	query = wyszukaj.get()
-#	webbrowser.get('firefox').open('https://google.com/search?q=%s' % query)
 def center_window(width=745, height=120):
-    # get screen width and height
     	screen_width = root.winfo_screenwidth()
     	screen_height = root.winfo_screenheight()
-
-    # calculate position x and y coordinates
     	x = (screen_width/2) - (width/2)
    	y = (screen_height/2) - (height/2)
     	root.geometry('%dx%d+%d+%d' % (width, height, x, y))
-center_window(745, 120)
+center_window(560, 320)
+font = tkFont.Font(size=25)
 def weather():
 	webbrowser.open('https://www.twojapogoda.pl/')
 def info():
@@ -28,19 +25,16 @@ def wylacz():
 	os.system("shutdown now")
 def inter():
 	webbrowser.open('https://google.com')
+internet = Button(root, text="Internet", command=inter, padx=70, pady=50, bg='#2b2b2b', fg='#fff', font=font)
+pogoda = Button(root, text="Pogoda", command=weather, padx=75, pady=50, bg='#2b2b2b', fg='#fff', font=font)
+wiadomosci = Button(root, text="Wloszczowa", command=info, padx=37, pady=50, bg='#2b2b2b', fg='#fff', font=font)
+wylacz = Button(root, text="Wylacz", command=wylacz, padx=78, pady=50, bg='#2b2b2b', fg='#fff', font=font)
+bo = Label(root, text="Biuro Obslugi Klienta: Milosz", pady=5, fg='#fff', bg='#2b2b2b')
 
-#wyszukaj = Entry(root, width="92", borderwidth="5")
-#wysz_Button = Button(root, text="Wyszukaj", command=search, padx=92)
-internet = Button(root, text="Internet", command=inter, padx=65, pady=50)
-pogoda = Button(root, text="Pogoda", command=weather, padx=66, pady=50)
-wiadomosci = Button(root, text="Wloszczowa", command=info, padx=50, pady=50)
-wylacz = Button(root, text="Wylacz", command=wylacz, padx=67, pady=50)
-
-#wyszukaj.grid(row=0, column=0, columnspan=4)
-#wysz_Button.grid(row=1, column=0, columnspan=4)
-internet.grid(row=2, column=1)
-pogoda.grid(row=2,column=2)
-wiadomosci.grid(row=2,column=3)
-wylacz.grid(row=2,column=0)
+internet.grid(row=1, column=0)
+pogoda.grid(row=1,column=1)
+wiadomosci.grid(row=2,column=0)
+wylacz.grid(row=2,column=1)
+bo.grid(row=3,column=0, columnspan=2)
 
 root.mainloop()
