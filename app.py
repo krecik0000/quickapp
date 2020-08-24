89ded1daac69ab4fa75cc4539e0750473f5f0df5
krecik0000 <milo04v2@gmail.com>
2020-08-25 00:45:58 +0200

    req

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from Tkinter import *
-import tkFont
+from tkinter import *
+import tkinter.font as TkFont
 import webbrowser
 import os
 #from googlesearch import *
@@ -7,16 +7,19 @@ import os
 root = Tk()
 root.title("Menu")
 root.configure(bg='#2b2b2b')
-#root.geometry('745x120')
+root.geometry('550x320')
 root.resizable(width=0, height=0)
+'''
 def center_window(width=745, height=120):
     	screen_width = root.winfo_screenwidth()
     	screen_height = root.winfo_screenheight()
     	x = (screen_width/2) - (width/2)
-   	y = (screen_height/2) - (height/2)
+		y = (screen_height/2) - (height/2)
     	root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
 center_window(560, 320)
-font = tkFont.Font(size=25)
+'''
+font = TkFont.Font(size=25)
 def weather():
 	webbrowser.open('https://www.twojapogoda.pl/')
 def info():
@@ -25,7 +28,7 @@ def wylacz():
 	os.system("shutdown now")
 def inter():
 	webbrowser.open('https://google.com')
-internet = Button(root, text="Internet", command=inter, padx=70, pady=50, bg='#2b2b2b', fg='#fff', font=font)
+internet = Button(root, text="Internet", command=inter, padx=74, pady=50, bg='#2b2b2b', fg='#fff', font=font)
 pogoda = Button(root, text="Pogoda", command=weather, padx=75, pady=50, bg='#2b2b2b', fg='#fff', font=font)
 wiadomosci = Button(root, text="Wloszczowa", command=info, padx=37, pady=50, bg='#2b2b2b', fg='#fff', font=font)
 wylacz = Button(root, text="Wylacz", command=wylacz, padx=78, pady=50, bg='#2b2b2b', fg='#fff', font=font)
